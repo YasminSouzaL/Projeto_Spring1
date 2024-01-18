d321512fbafd1e2b0285ca830a87b4d35ee41741
Yasmin Souza <souzasmimi@gmail.com>
2024-01-18 08:30:50 -0300

    gerando a base Categoria

--- src/main/java/com/srayasmin/projetomc/ProjetomcApplication.java
+++ src/main/java/com/srayasmin/projetomc/ProjetomcApplication.java
@@ -3,7 +3,6 @@ package com.srayasmin.projetomc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ProjetomcApplication {
     public static void main(String[] args) {
