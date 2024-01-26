3b36e8b15a48ee570ddf2200113b40ea05410f36
Yasmin Souza <souzasmimi@gmail.com>
2024-01-25 21:58:49 -0300

    SetviceandRepositores

--- src/main/java/com/srayasmin/projetomc/controller/CategoriaController.java
+++ src/main/java/com/srayasmin/projetomc/controller/CategoriaController.java
@@ -1,23 +1,23 @@
 package com.srayasmin.projetomc.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.srayasmin.projetomc.domain.Categoria;
+import com.srayasmin.projetomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaController {
-    @GetMapping
-    public List<Categoria> listar(){
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-        
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+    @Autowired
+    private CategoriaService service;
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id){
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
