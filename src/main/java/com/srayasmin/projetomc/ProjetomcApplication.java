29b907e6d762704df15ff59db4f1b7288bbdd8ca
Yasmin Souza <souzasmimi@gmail.com>
2024-01-25 21:01:44 -0300

    Repository e Service para Categoria

--- src/main/java/com/srayasmin/projetomc/ProjetomcApplication.java
+++ src/main/java/com/srayasmin/projetomc/ProjetomcApplication.java
@@ -2,9 +2,8 @@ package com.srayasmin.projetomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class ProjetomcApplication {
+public class ProjetomcApplication{
     public static void main(String[] args) {
         SpringApplication.run(ProjetomcApplication.class, args);
     }
--- src/main/java/com/srayasmin/projetomc/domain/Categoria.java
+++ src/main/java/com/srayasmin/projetomc/domain/Categoria.java
@@ -1,10 +1,11 @@
 package com.srayasmin.projetomc.domain;
 
-import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
+
 
 @Entity
 public class Categoria implements Serializable{
