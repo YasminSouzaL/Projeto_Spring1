04f151821132fe178f5ad613c2c346366fa348ad
Yasmin Souza <souzasmimi@gmail.com>
2024-01-29 15:50:10 -0300

    Add Serializable interface to Cidade and Estado classes

--- src/main/java/com/srayasmin/projetomc/domain/Cidade.java
+++ src/main/java/com/srayasmin/projetomc/domain/Cidade.java
@@ -1,6 +1,9 @@
 package com.srayasmin.projetomc.domain;
 
-public class Cidade {
+import java.io.Serializable;
+
+public class Cidade implements Serializable{
+    private static final long serialVersionUID = 1L;
     private Integer id;
     private String nome;
     // Cidade tem um estado:
@@ -21,4 +24,27 @@ public class Cidade {
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
     public Estado getEstado() {return estado;}
+
+    //hasCode and equals:
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        //result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        //result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {return true;}
+        if(obj == null) {return false;}
+        if(getClass() != obj.getClass()) {return false;}
+        Cidade other = (Cidade) obj;
+        if(getId() == null) {
+            if(other.getId() != null) {return false;}
+        } else if(!getId().equals(other.getId())) {return false;}
+        return true;
+    }
 }
--- src/main/java/com/srayasmin/projetomc/domain/Estado.java
+++ src/main/java/com/srayasmin/projetomc/domain/Estado.java
@@ -1,9 +1,11 @@
 package com.srayasmin.projetomc.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Estado {
+public class Estado implements Serializable{
+    private static final long serialVersionUID = 1L;
     private Integer id;
     private String nome;
 
