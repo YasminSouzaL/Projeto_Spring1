8b78434b02e46c799fac45dbda4cadd64309c89e
Yasmin Souza <souzasmimi@gmail.com>
2024-01-30 17:44:43 -0300

    Add entity annotations and mappings for Pagamento and Pedido classes

--- src/main/java/com/srayasmin/projetomc/domain/Pagamento.java
+++ src/main/java/com/srayasmin/projetomc/domain/Pagamento.java
@@ -2,13 +2,28 @@ package com.srayasmin.projetomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.srayasmin.projetomc.domain.enums.EstadoPagamento;
 
+
+
+@Entity
 public class Pagamento implements Serializable{
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private EstadoPagamento estado;
-
+    @OneToOne
+    @JoinColumn(name = "pedido_id")
+    @MapsId
     private Pedido pedido;
 
     public Pagamento(){}
