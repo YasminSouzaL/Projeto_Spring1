542ab4b70c819ae82183d9bcd8851cfa23c96546
Yasmin Souza <souzasmimi@gmail.com>
2024-02-01 22:57:39 -0300

    Adicionado método insert na classe CategoriaController

--- src/main/java/com/srayasmin/projetomc/services/CategoriaService.java
+++ src/main/java/com/srayasmin/projetomc/services/CategoriaService.java
@@ -29,5 +29,10 @@ public class CategoriaService {
     public Categoria buscar(Integer id) {
         throw new UnsupportedOperationException("Unimplemented method 'buscar'");
     }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
  
 }
