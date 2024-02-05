d8d6267039889d0f5b00e392e468cef426ff1744
Yasmin Souza <souzasmimi@gmail.com>
2024-02-05 16:28:54 -0300

    Adicionado código para salvar Estado e Cidade e criar Cliente

--- src/main/java/com/srayasmin/projetomc/ProjetomcApplication.java
+++ src/main/java/com/srayasmin/projetomc/ProjetomcApplication.java
@@ -84,7 +84,7 @@ public class ProjetomcApplication implements CommandLineRunner{
         //Salvar Estado e Cidade
         estadoRepository.saveAll(Arrays.asList(est1, est2));
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-
+        
         //Criar Cliente
         Cliente cli1 = new Cliente(null, "Maria Silva", "maria@email", "777777", TipoCliente.PESSOAFISICA);
         cli1.getTelefones().addAll(Arrays.asList("777777", "888888"));
