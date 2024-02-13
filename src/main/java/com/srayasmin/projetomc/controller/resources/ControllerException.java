de50511b41e9f2bf16c58f28b3b8060806a70620
Yasmin Souza <souzasmimi@gmail.com>
2024-02-13 14:48:23 -0300

    Adiciona tratamento de exceção para validação de argumentos

--- src/main/java/com/srayasmin/projetomc/controller/resources/ControllerException.java
+++ src/main/java/com/srayasmin/projetomc/controller/resources/ControllerException.java
@@ -1,8 +1,11 @@
 package com.srayasmin.projetomc.controller.resources;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity; // Add this import statement
+import org.springframework.validation.FieldError;
 
 import com.srayasmin.projetomc.services.exceptions.DataIntegrityException;
 import com.srayasmin.projetomc.services.exceptions.ObjectNotFoundException;
@@ -18,6 +21,15 @@ public class ControllerException {
 
     public ResponseEntity<StandarError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
         StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+        for(FieldError x : e.getBindingResult().getFieldErrors()){
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
---
+++ src/main/java/com/srayasmin/projetomc/controller/resources/ValidationError.java
@@ -0,0 +1,14 @@
+package com.srayasmin.projetomc.controller.resources;
+
+public class ValidationError {
+
+    public ValidationError(int value, String string, long timeMillis) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void addError(String field, String defaultMessage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addError'");
+    }
+
+}
